fbf377f5509ad76256ce76cd785688116f8b6794
hust-quangtm <quang.tm176858@gmail.com>
2021-05-20 16:46:35 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0xtIgUDkLrwxnQ7wEy5Sl4RrnuYGNYy4",
+  authDomain: "fir-sample-ebd48.firebaseapp.com",
+  projectId: "fir-sample-ebd48",
+  storageBucket: "fir-sample-ebd48.appspot.com",
+  messagingSenderId: "501525580831",
+  appId: "1:501525580831:web:5cf836dc6bb0fb488bd92a"
+};
+
+firebase.initializeApp(firebaseConfig);
